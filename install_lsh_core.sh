2b61c4b475c54327376e70900df2c98288dd4891
Rajmond Berisha <rajmond_berisha@hotmail.com>
2020-02-20 22:41:18 +0100

    Update install_lsh_core.sh

--- install_lsh_core.sh
+++ install_lsh_core.sh
@@ -58,8 +58,8 @@ prepare_db ()
                         echo -e "${GREEN}Done!\n ${NC}"
                         
                         echo -e "${GREEN} \nUploading Lisk snapshot to DB\n ${NC}"
-                        wget http://snapshots.lisk.io.s3-eu-west-1.amazonaws.com/lisk/testnet/lisk_test_backup-10196059.gz
-                        gzip --decompress --to-stdout ./lisk_test_backup-10196059.gz | psql lisk_test -U leasehold
+                        #wget http://snapshots.lisk.io.s3-eu-west-1.amazonaws.com/lisk/testnet/lisk_test_backup-10196059.gz
+                        #gzip --decompress --to-stdout ./lisk_test_backup-10196059.gz | psql lisk_test -U leasehold
                         echo -e "${GREEN}Done!\n ${NC}"
                 fi
 
